bfc68e66286a3e869adafb45c3599c7d8113ee7f
vsgirelli <vavi_sg@hotmail.com>
2021-04-01 16:49:34 -0300

    Adding comments to clarify the code and to organize the report

--- decisionTree.py
+++ decisionTree.py
@@ -3,7 +3,8 @@ from node import Node
 from math import log2
 from utils import *
 
-
+# The verification of whether all nodes belong to the same class.
+# Used on the tree generation.
 def isOnlyOneClass(training_data, target_column):
   y = training_data[target_column]
   classes = y.unique()
@@ -19,6 +20,8 @@ def getMajorityClass(training_data, target_colum):
   #get most frequent element in a pandas series
   return (y.mode().iloc[0])
 
+# We call "base case" the case on which all nodes belong to the same class
+# or the attribute list is empty
 def isBaseCase(training_data, target_column, attributes):
   if (isOnlyOneClass(training_data, target_column)) or (len(attributes) == 0):
     return True
@@ -29,17 +32,18 @@ def decisionTreeClassifier(training_data, attributes, target_column):
   node = Node()
   node_entropy = entropy(target_column, training_data)
 
-  #if all examples in the current dataset belong to the same class or attribute list is empty, return the node as a leaf
+  # If all examples in the current dataset belong to the same class
+  # or the attribute list is empty, return the node as a leaf
   if isBaseCase(training_data, target_column, attributes):
     node.is_leaf = True
     node.label = getMajorityClass(training_data, target_column)
     return node
-  #select attribute with best split criteria
+  # Select attribute with best split criteria (information gain)
   else:
     best_attribute = attributeSelection(training_data, attributes, target_column, node_entropy)
     node.attribute = best_attribute
-    attributes.remove(best_attribute) #remove used attribute from attributes list
-    #for each unique value of the selected attribute, 
+    attributes.remove(best_attribute) # remove used attribute from attributes list
+    # for each unique value of the selected attribute, 
     attribute_values = get_unique_values(training_data, best_attribute)
     for attribute_value in attribute_values:
       value_sub_set = training_data[training_data[best_attribute] == attribute_value]
--- utils.py
+++ utils.py
@@ -2,11 +2,14 @@ import pandas as pd
 import random
 from math import sqrt
 
+# Get the unique values of an attribute, and it's used to create the child nodes.
 def get_unique_values(dataset, target_column):
   y = training_data[target_column]
   unique_values = y.unique()
   return unique_values
 
+# Calculates the class' probability.
+# This information is used on the entropy calculation.
 def prob_of_class(target_column, class_name, data_set):
   # get all unique values from a given column
   y = data_set[target_column]
@@ -17,7 +20,7 @@ def prob_of_class(target_column, class_name, data_set):
   # pd_series = pd.Series(y)
   return prob
 
-# calculate the entropy for the split in the dataset
+# Calculate the entropy for the split in the dataset
 def entropy(target_column, data_set):
   bits = 0
   y = data_set[target_column]
@@ -28,8 +31,9 @@ def entropy(target_column, data_set):
     bits -= class_prob * log2(class_prob)
 
   return bits
-  
 
+# Bootstrap for selecting subsets from the training dataset.
+# Each bootstrap is applied on the training phase of a given tree.
 def bootstrap(data):
   # the sample size will be the number of rows in the df
   sampleSize = data.shape[0]
@@ -38,17 +42,21 @@ def bootstrap(data):
 
   return boot
 
+# The information gain.
+# Used in the best attribute selection.
 def information_gain(current_entropy, new_entropy):
   return current_entropy - new_entropy
 
+# Selection of the best attribute considering its information gain value.
+# The attribute with the highest entropy is the one selected to be the current node.
 def attributeSelection(training_data, attributes_list, target_column, current_entropy):
   # randomly selecting the square root of #attributes
   m = sqrt(len(attributes_list))
   header = random.sample(attributes_list, round(m))
   data_length = len(training_data)
   information_gain_values = []
-  
-  #go through each attribute and calculate the entropy 
+
+  #go through each attribute and calculate the entropy
   for attribute in header:
     #get each possible value for the given attribute
     unique_values = training_data[attribute].unique()
@@ -65,6 +73,6 @@ def attributeSelection(training_data, attributes_list, target_column, current_en
     information_gain_values.append(information_gain(current_entropy, attribute_info))
 
   #print(header[entropyValues.index(min(entropyValues))])
-  
+
   #return the attribute that maximizes the information gain
   return header[information_gain_values.index(max(information_gain_values))]
