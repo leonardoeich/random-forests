2c3ac3281802f6d453c7ecf41811bcada6c2a4bf
Leonardo Eich <leonardoeich@hotmail.com>
2021-04-06 15:17:30 -0300

    creating data type files and finishing decisionTree algorithm

--- decisionTree.py
+++ decisionTree.py
@@ -28,7 +28,16 @@ def isBaseCase(training_data, target_column, attributes):
   else:
     return False
 
-def decisionTreeClassifier(training_data, attributes, target_column):
+# check if all values are of an attribute are in the current data
+def all_values_exist(current_values, possible_values):
+  exists = True
+  for value in possible_values:
+    if value not in current_values:
+      exists = False
+  return exists
+
+
+def decisionTreeClassifier(training_data, attributes, target_column, data_dict, data_types):
   node = Node()
   node_entropy = entropy(target_column, training_data)
 
@@ -37,18 +46,20 @@ def decisionTreeClassifier(training_data, attributes, target_column):
   if isBaseCase(training_data, target_column, attributes):
     node.is_leaf = True
     node.label = getMajorityClass(training_data, target_column)
-    return node
   # Select attribute with best split criteria (information gain)
   else:
     best_attribute = attributeSelection(training_data, attributes, target_column, node_entropy)
     node.attribute = best_attribute
-    attributes.remove(best_attribute) # remove used attribute from attributes list
-    # for each unique value of the selected attribute, 
+    # remove used attribute from attributes list
+    attributes.remove(best_attribute)
     attribute_values = get_unique_values(training_data, best_attribute)
-    for attribute_value in attribute_values:
-      value_sub_set = training_data[training_data[best_attribute] == attribute_value]
-      if len(value_sub_set) > 0:
-        node.branches.append(decisionTreeClassifier(value_sub_set, attributes, target_column))
-      else:
-        ##TODO
-        pass
+    #check if all attributes values are represented
+    if all_values_exist(attribute_values, data_dict[best_attribute]):
+      for attribute_value in attribute_values:
+        value_sub_set = training_data[training_data[best_attribute] == attribute_value]
+        node.branches.append(decisionTreeClassifier(value_sub_set, attributes, target_column, data_dict, data_types))
+    else:
+      node.is_leaf = True
+      node.label = getMajorityClass(training_data, target_column)
+  
+  return node
